a2554c90aa13d2dd47a73ef51b1dd8a4d56217bb
laeticia-pierre <laeticia.pierre06200@gmail.com>
2016-12-17 19:46:35 +0100

    #3 Squelette protocole d’anneau virtuel

--- src/node/ChordPeer.java
+++ src/node/ChordPeer.java
@@ -1,14 +1,58 @@
 package node;
 
 public class ChordPeer {
-	/*
-	 * Chaque pair de la classe ChordPeer connaitra ici :
-		son identifiant myIdmyId
-		la référence de son successeur succsucc.
-		la référence de son prédécesseur predpred.
+
+	/**
+	 * Determine la place sur l'anneau
+	 * Peut etre : fonction de hachage sur lâ€™IP du pair ou lâ€™identifiant de lâ€™utilisateur
+	 * ou encore alÃ©atoire
 	 */
+	private int key;
+	/**
+	 * Paire successeur dans l'anneau principal 
+	 * A maintenir
+	 */
+	//TODO ou une ArrayList ?
+	private ChordPeer successeur;
+	/**
+	 * Paire predecesseur dans l'anneau principal 
+	 * A maintenir
+	 */
+	//TODO ou une ArrayList ?
+	private ChordPeer predecesseur;
 	
+	/**
+	 * Retourne l'emplacement de key dans lâ€™anneau
+	 * @param key du pair souhaitant rejoindre lâ€™anneau
+	 * @return int
+	 */
+	public int dindMainChord(long key)  {
+		return 0;
+	}
 	
+	/**
+	 * Permet au paire courant de s'inserer
+	 * Initialisation des references distantes(predecesseur et successeur)
+	 */
+	public void joinMainChord() {
+		
+	}
 	
+	/**
+	 * Met Ã  jour les liens de lâ€™anneau virtuel 
+	 * i,e notre predecesseur devra des lors pointer sur notre successeur
+	 */
+	public void leaveMainChord() {
+		
+	}
+	
+	/**
+	 * Faire circuler un message vers son/ses sucesseurs
+	 * On veillera a ne pas transmettre le meme message deux fois
+	 * @param data message a faire circuler
+	 */
+	public void forwardMessage(String data) {
+		
+	}
 	
 }
