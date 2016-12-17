fab4683e32c4841b3cbac1854ae5e13aaa0ddfbf
laeticia-pierre <laeticia.pierre06200@gmail.com>
2016-12-17 23:35:38 +0100

    #4 Squelette enoncé salon conversation

---
+++ src/node/SalonConversation.java
@@ -0,0 +1,39 @@
+package node;
+
+import java.util.ArrayList;
+
+public class SalonConversation { // Ou GestionnaireSalons pour ces methodes, a voir
+	
+	/** 
+	 * Renvoie la liste des identifiants des salles de chat
+	 * @return ArrayList<Long>
+	 */
+	public ArrayList<Long> getChatRoomsList()  {
+		return null;
+	}
+
+	/**
+	 * Rejoint la salle si elle existe, la cree sinon
+	 * @param chatkey
+	 */
+	public void joinChatRoom(long chatkey) {
+		
+	}
+	
+	/**
+	 * Envoie un message dans le salon chatkey
+	 * @param s message
+	 * @param chatkey
+	 */
+	public void sendToChatRoom(String s, long chatkey) {
+	
+	}
+	
+	/**
+	 * Reference a un tampon stockant les messages venant du salon
+	 */
+	public void readChatRoom(long chatkey) {
+		
+	}
+	
+}
